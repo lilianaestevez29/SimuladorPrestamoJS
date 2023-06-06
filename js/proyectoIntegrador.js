fbd0fe4c75cc66d1f47a7e18d39f60077f58cb42
liliana <lilianabiogalenic@gmail.com>
2023-06-06 16:25:09 -0300

    se añadió la estructura while al proyecto

--- js/proyectoIntegrador.js
+++ js/proyectoIntegrador.js
@@ -1,16 +1,28 @@
 function simularPrestamo() {
-    let monto = parseInt(prompt("Ingrese el monto del préstamo:"));
-    let plazo = parseInt(prompt("Ingrese el plazo en meses:  "));
-        if (monto <= 0 || plazo <= 0) {
-        alert("El monto y el plazo deben ser mayores a cero.");
-        } else {
-        let tasaInteresAnual = 0.1; 
-        let tasaInteresMensual = tasaInteresAnual / 12;
+    let repetir = true;
+
+    while (repetir) {
+        let nombre = prompt("Ingresa Nombre y Apellido");
+        let monto =  prompt("Indique el monto a solicitar");
+        let plazo = prompt("Indique el plazo a pagar");
         
-        let cuotaMensual = monto * (tasaInteresMensual / (1 - Math.pow(1 + tasaInteresMensual, -plazo)));
+        if (!nombre || !monto || !plazo) {
+            alert("No ingresó los datos requeridos");
+            } else if (monto <= 0 || plazo <= 0) { 
+                alert("El monto y el plazo deben ser mayores a cero.");
+            } else {
+                let InteresAnual = 0.12;
+                let InteresMensual = InteresAnual / 12;
+                let cuotaMensual = monto * (InteresMensual / (1 - Math.pow(1 + InteresMensual, -plazo)));
         
-        alert("El monto a pagar mensualmente es: $" + cuotaMensual.toFixed(2));
-        }
-  }
+                alert("El monto a pagar mensualmente es: $" + cuotaMensual.toFixed(2));
+            }
+      
+      repetir = confirm("¿Deseas simular otro préstamo?"); 
+    }
+}
+ 
+
+
   
   
